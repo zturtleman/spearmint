7af52d95121077225f6a24f769df89903dc54b38
Zack Middleton <zturtleman@gmail.com>
2013-01-11 01:40:01 -0600

    Fix portal in q3dm0
    Restores original triangles code in R_PlaneForSurface, it was changed in "OpenGL1 renderer fixes" commit.

--- code/renderer/tr_main.c
+++ code/renderer/tr_main.c
@@ -818,7 +818,9 @@ R_PlaneForSurface
 =============
 */
 void R_PlaneForSurface (surfaceType_t *surfType, cplane_t *plane) {
+	srfTriangles_t  *tri;
 	srfPoly_t		*poly;
+	drawVert_t    *v1, *v2, *v3;
 	vec4_t			plane4;
 
 	if (!surfType) {
@@ -828,7 +830,13 @@ void R_PlaneForSurface (surfaceType_t *surfType, cplane_t *plane) {
 	}
 	switch (*surfType) {
 	case SF_TRIANGLES:
-		*plane = ((srfGeneric_t *)surfType)->plane;
+		tri = (srfTriangles_t *)surfType;
+		v1 = tri->verts + tri->indexes[0];
+		v2 = tri->verts + tri->indexes[1];
+		v3 = tri->verts + tri->indexes[2];
+		PlaneFromPoints( plane4, v1->xyz, v2->xyz, v3->xyz );
+		VectorCopy( plane4, plane->normal );
+		plane->dist = plane4[3];
 		return;
 	case SF_POLY:
 		poly = (srfPoly_t *)surfType;
