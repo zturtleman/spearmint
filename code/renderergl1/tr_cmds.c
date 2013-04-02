b9dc9cfaa9b4c45b24a248e111ea243a1cf4bc8d
Zack Middleton <zturtleman@gmail.com>
2013-04-02 12:35:58 -0500

    Fix and improve GetWaterFog
    It was broken in opengl1 (always returned earily if world was loaded).
    Actually check all brush surfaces (not just the first surface multiple times).
    Always set values even if map has no water fog.

--- code/renderergl1/tr_cmds.c
+++ code/renderergl1/tr_cmds.c
@@ -479,65 +479,59 @@ RE_GetWaterFog
 ====================
 */
 void RE_GetWaterFog( const vec3_t origin, fogType_t *type, vec3_t color, float *depthForOpaque, float *density ) {
-	int					bmodelNum;
-	int					surfaceNum;
-	const bmodel_t		*bmodel;
-	const msurface_t	*surface;
+	fogType_t			fogType = FT_NONE;
+	vec3_t				fogColor = { 0, 0, 0 };
+	float				fogDepthForOpaque = 0;
+	float				fogDensity = 0;
 
 	if ( tr.world ) {
-		if (type) {
-			*type = FT_NONE;
-		}
+		int					bmodelNum;
+		int					surfaceNum;
+		const bmodel_t		*bmodel;
+		const msurface_t	*surface;
 
-		if (color) {
-			VectorSet( color, 0, 0, 0 );
-		}
+		for ( bmodelNum = 0, bmodel = tr.world->bmodels; bmodelNum < tr.world->numBModels; bmodelNum++, bmodel++ ) {
 
-		if (depthForOpaque) {
-			*depthForOpaque = 0;
-		}
+			if ( !R_PointInBrush( origin, bmodel ) )
+				continue;
 
-		if (density) {
-			*density = 0;
-		}
+			for ( surfaceNum = 0, surface = bmodel->firstSurface; surfaceNum < bmodel->numSurfaces; surfaceNum++, surface++ ) {
+				if ( !surface->shader ) {
+					continue;
+				}
 
-		return;
+				if ( surface->shader->waterFogParms.fogType != FT_NONE
+						|| surface->shader->waterFogParms.color[0]
+						|| surface->shader->waterFogParms.color[1]
+						|| surface->shader->waterFogParms.color[2] ) {
+					fogType = surface->shader->waterFogParms.fogType;
+					VectorCopy( surface->shader->waterFogParms.color, fogColor );
+					fogDepthForOpaque = surface->shader->waterFogParms.depthForOpaque;
+					fogDensity = surface->shader->waterFogParms.density;
+					break;
+				}
+			}
+
+			if ( surfaceNum != bmodel->numSurfaces ) {
+				break;
+			}
+		}
 	}
 
-	for ( bmodelNum = 0, bmodel = tr.world->bmodels; bmodelNum < tr.world->numBModels; bmodelNum++, bmodel++ ) {
+	if (type) {
+		*type = fogType;
+	}
 
-		if ( !R_PointInBrush( origin, bmodel ) )
-			continue;
+	if (color) {
+		VectorCopy( fogColor, color );
+	}
 
-		for ( surfaceNum = 0, surface = bmodel->firstSurface; surfaceNum < bmodel->numSurfaces; surfaceNum++, surface ) {
-			if ( !surface->shader ) {
-				continue;
-			}
+	if (depthForOpaque) {
+		*depthForOpaque = fogDepthForOpaque;
+	}
 
-			if ( surface->shader->waterFogParms.fogType != FT_NONE
-					|| surface->shader->waterFogParms.color[0]
-					|| surface->shader->waterFogParms.color[1]
-					|| surface->shader->waterFogParms.color[2] ) {
-
-				if (type) {
-					*type = surface->shader->waterFogParms.fogType;
-				}
-
-				if (color) {
-					VectorCopy( surface->shader->waterFogParms.color, color );
-				}
-
-				if (depthForOpaque) {
-					*depthForOpaque = surface->shader->waterFogParms.depthForOpaque;
-				}
-
-				if (density) {
-					*density = surface->shader->waterFogParms.density;
-				}
-
-				return;
-			}
-		}
+	if (density) {
+		*density = fogDensity;
 	}
 }
 
--- code/renderergl2/tr_cmds.c
+++ code/renderergl2/tr_cmds.c
@@ -525,65 +525,59 @@ RE_GetWaterFog
 ====================
 */
 void RE_GetWaterFog( const vec3_t origin, fogType_t *type, vec3_t color, float *depthForOpaque, float *density ) {
-	int					bmodelNum;
-	int					surfaceNum;
-	const bmodel_t		*bmodel;
-	const msurface_t	*surface;
+	fogType_t			fogType = FT_NONE;
+	vec3_t				fogColor = { 0, 0, 0 };
+	float				fogDepthForOpaque = 0;
+	float				fogDensity = 0;
 
-	if ( !tr.world ) {
-		if (type) {
-			*type = FT_NONE;
+	if ( tr.world ) {
+		int					bmodelNum;
+		int					surfaceNum;
+		const bmodel_t		*bmodel;
+		const msurface_t	*surface;
+
+		for ( bmodelNum = 0, bmodel = tr.world->bmodels; bmodelNum < tr.world->numBModels; bmodelNum++, bmodel++ ) {
+
+			if ( !R_PointInBrush( origin, bmodel ) )
+				continue;
+
+			for ( surfaceNum = 0, surface = tr.world->surfaces + bmodel->firstSurface; surfaceNum < bmodel->numSurfaces; surfaceNum++, surface++ ) {
+				if ( !surface->shader ) {
+					continue;
+				}
+
+				if ( surface->shader->waterFogParms.fogType != FT_NONE
+						|| surface->shader->waterFogParms.color[0]
+						|| surface->shader->waterFogParms.color[1]
+						|| surface->shader->waterFogParms.color[2] ) {
+					fogType = surface->shader->waterFogParms.fogType;
+					VectorCopy( surface->shader->waterFogParms.color, fogColor );
+					fogDepthForOpaque = surface->shader->waterFogParms.depthForOpaque;
+					fogDensity = surface->shader->waterFogParms.density;
+					break;
+				}
+			}
+
+			if ( surfaceNum != bmodel->numSurfaces ) {
+				break;
+			}
 		}
-
-		if (color) {
-			VectorSet( color, 0, 0, 0 );
-		}
-
-		if (depthForOpaque) {
-			*depthForOpaque = 0;
-		}
-
-		if (density) {
-			*density = 0;
-		}
-
-		return;
 	}
 
-	for ( bmodelNum = 0, bmodel = tr.world->bmodels; bmodelNum < tr.world->numBModels; bmodelNum++, bmodel++ ) {
+	if (type) {
+		*type = fogType;
+	}
 
-		if ( !R_PointInBrush( origin, bmodel ) )
-			continue;
+	if (color) {
+		VectorCopy( fogColor, color );
+	}
 
-		for ( surfaceNum = 0, surface = tr.world->surfaces + bmodel->firstSurface; surfaceNum < bmodel->numSurfaces; surfaceNum++, surface ) {
-			if ( !surface->shader ) {
-				continue;
-			}
+	if (depthForOpaque) {
+		*depthForOpaque = fogDepthForOpaque;
+	}
 
-			if ( surface->shader->waterFogParms.fogType != FT_NONE
-					|| surface->shader->waterFogParms.color[0]
-					|| surface->shader->waterFogParms.color[1]
-					|| surface->shader->waterFogParms.color[2] ) {
-
-				if (type) {
-					*type = surface->shader->waterFogParms.fogType;
-				}
-
-				if (color) {
-					VectorCopy( surface->shader->waterFogParms.color, color );
-				}
-
-				if (depthForOpaque) {
-					*depthForOpaque = surface->shader->waterFogParms.depthForOpaque;
-				}
-
-				if (density) {
-					*density = surface->shader->waterFogParms.density;
-				}
-
-				return;
-			}
-		}
+	if (density) {
+		*density = fogDensity;
 	}
 }
 
