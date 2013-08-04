d17b1a113eb19f93a0b52810243f85f21b98d69a
Zack Middleton <zturtleman@gmail.com>
2013-08-04 18:09:15 -0500

    Don't calculate fogDistanceVector before checking if fog is enabled

--- code/renderergl1/tr_shade_calc.c
+++ code/renderergl1/tr_shade_calc.c
@@ -959,13 +959,6 @@ void RB_CalcFogTexCoords( float *st ) {
 		return;
 	}
 
-	// all fogging distance is based on world Z units
-	VectorSubtract( backEnd.or.origin, backEnd.viewParms.or.origin, local );
-	fogDistanceVector[0] = -backEnd.or.modelMatrix[2];
-	fogDistanceVector[1] = -backEnd.or.modelMatrix[6];
-	fogDistanceVector[2] = -backEnd.or.modelMatrix[10];
-	fogDistanceVector[3] = DotProduct( local, backEnd.viewParms.or.axis[0] );
-
 	if ( tess.shader->isSky ) {
 		fogType = tr.skyFogType;
 		tcScale = tr.skyFogTcScale;
@@ -985,6 +978,13 @@ void RB_CalcFogTexCoords( float *st ) {
 	if ( fogType == FT_NONE ) {
 		return;
 	}
+
+	// all fogging distance is based on world Z units
+	VectorSubtract( backEnd.or.origin, backEnd.viewParms.or.origin, local );
+	fogDistanceVector[0] = -backEnd.or.modelMatrix[2];
+	fogDistanceVector[1] = -backEnd.or.modelMatrix[6];
+	fogDistanceVector[2] = -backEnd.or.modelMatrix[10];
+	fogDistanceVector[3] = DotProduct( local, backEnd.viewParms.or.axis[0] );
 
 	// level-wide fogging
 	if ( tess.shader->isSky || fog->originalBrushNumber < 0 ) {
