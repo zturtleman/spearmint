zturtleman/spearmint

commit a1d7aeec6088b7c96c366b413e3c15d8f3dc0b4a

Clean up adding and removing commands from VMs
Auto remove commands added by game and cgame VMs when shuting down the VM.
Game VM no longer manually removes commands.
Save pointer to function to run VM command. No longer have to ask each VM if they want to handle a cmd and then forward to server, now just give cmd to VM that added cmd.
Moved forwarding commands to server into cgame. (cgame adds the commands; so it should handle them. Allows Cmd_ExecuteString to be cleaner.)
Only allow VM to remove commands it added (previously VMs could remove each others commands).

code/cgame/cg_consolecmds.c
@@ -701,6 +701,7 @@ Cmd_Argc() / Cmd_Argv()
 =================
 */
 qboolean CG_ConsoleCommand( void ) {
+	char		buffer[BIG_INFO_STRING];
 	const char	*cmd;
 	int		i;
 	int		localPlayerNum;
@@ -722,18 +723,24 @@ qboolean CG_ConsoleCommand( void ) {
 		}
 	}
 
-	if ( localPlayerNum != 0 )
-		return qfalse;
-
-	for ( i = 0 ; i < ARRAY_LEN( commands ) ; i++ ) {
-		if ( !Q_stricmp( cmd, commands[i].cmd )) {
-			if ( ( commands[i].flags & CMD_INGAME ) && !cg.snap ) {
-				CG_Printf("You must be in game to use command \"%s\"\n", cmd);
+	if ( localPlayerNum == 0 ) {
+		for ( i = 0 ; i < ARRAY_LEN( commands ) ; i++ ) {
+			if ( !Q_stricmp( cmd, commands[i].cmd )) {
+				if ( ( commands[i].flags & CMD_INGAME ) && !cg.snap ) {
+					CG_Printf("You must be in game to use command \"%s\"\n", cmd);
+					return qtrue;
+				}
+				commands[i].function();
 				return qtrue;
 			}
-			commands[i].function();
-			return qtrue;
 		}
+	}
+
+	if ( cg.connected && trap_GetDemoState() != DS_PLAYBACK ) {
+		// the game server will interpret these commands
+		trap_LiteralArgs( buffer, sizeof ( buffer ) );
+		trap_SendClientCommand( buffer );
+		return qtrue;
 	}
 
 	return qfalse;

code/client/cl_cgame.c
@@ -318,15 +318,6 @@ void CL_SetNetFields( int entityStateSize, vmNetField_t *entityStateFields, int 
 					  playerStateFields, numPlayerStateFields, playerStateSize );
 }
 
-/*
-=====================
-CL_AddCgameCommand
-=====================
-*/
-void CL_AddCgameCommand( const char *cmdName ) {
-	Cmd_AddCommand( cmdName, NULL );
-}
-
 
 /*
 =====================
@@ -1091,6 +1082,8 @@ void CL_ShutdownCGame( void ) {
 	VM_Call( cgvm, CG_SHUTDOWN );
 	VM_Free( cgvm );
 	cgvm = NULL;
+
+	Cmd_RemoveCommandsByFunc( CL_GameCommand );
 }
 
 /*
@@ -1227,10 +1220,10 @@ intptr_t CL_CgameSystemCalls( intptr_t *args ) {
 		Cbuf_ExecuteTextSafe( args[1], VMA(2) );
 		return 0;
 	case CG_ADDCOMMAND:
-		CL_AddCgameCommand( VMA(1) );
+		Cmd_AddCommandSafe( VMA(1), CL_GameCommand );
 		return 0;
 	case CG_REMOVECOMMAND:
-		Cmd_RemoveCommandSafe( VMA(1) );
+		Cmd_RemoveCommandSafe( VMA(1), CL_GameCommand );
 		return 0;
 	case CG_SENDCLIENTCOMMAND:
 		CL_AddReliableCommand(VMA(1), qfalse);
@@ -1764,15 +1757,15 @@ void CL_InitCGame( void ) {
 ====================
 CL_GameCommand
 
-See if the current console command is claimed by the cgame
+Pass the current console command to cgame
 ====================
 */
-qboolean CL_GameCommand( void ) {
+void CL_GameCommand( void ) {
 	if ( !cgvm ) {
-		return qfalse;
+		return;
 	}
 
-	return VM_Call( cgvm, CG_CONSOLE_COMMAND, cls.realtime );
+	VM_Call( cgvm, CG_CONSOLE_COMMAND, cls.realtime );
 }
 
 /*

code/client/cl_main.c
@@ -1638,38 +1638,6 @@ void CL_Disconnect( qboolean showMainMenu ) {
 		noGameRestart = qfalse;
 }
 
-
-/*
-===================
-CL_ForwardCommandToServer
-
-adds the current command line as a clientCommand
-things like godmode, noclip, etc, are commands directed to the server,
-so when they are typed in at the console, they will need to be forwarded.
-===================
-*/
-void CL_ForwardCommandToServer( const char *string ) {
-	char	*cmd;
-
-	cmd = Cmd_Argv(0);
-
-	// ignore key up commands
-	if ( cmd[0] == '-' ) {
-		return;
-	}
-
-	if ( clc.demoplaying || clc.state < CA_CONNECTED || cmd[0] == '+' ) {
-		Com_Printf ("Unknown command \"%s" S_COLOR_WHITE "\"\n", cmd);
-		return;
-	}
-
-	if ( Cmd_Argc() > 1 ) {
-		CL_AddReliableCommand(string, qfalse);
-	} else {
-		CL_AddReliableCommand(cmd, qfalse);
-	}
-}
-
 /*
 ===================
 CL_RequestMotd

code/client/client.h
@@ -588,7 +588,7 @@ void CIN_CloseAllVideos(void);
 //
 void CL_InitCGame( void );
 void CL_ShutdownCGame( void );
-qboolean CL_GameCommand( void );
+void CL_GameCommand( void );
 void CL_CGameRendering( stereoFrame_t stereo );
 void CL_SetCGameTime( void );
 void CL_FirstSnapshot( void );

code/game/g_local.h
@@ -613,7 +613,6 @@ qboolean SpotWouldTelefrag( gentity_t *spot );
 //
 qboolean	ConsoleCommand( void );
 void G_RegisterCommands( void );
-void G_UnregisterCommands( void );
 void G_ProcessIPBans(void);
 qboolean G_FilterPacket (char *from);
 

code/game/g_main.c
@@ -613,8 +613,6 @@ void G_ShutdownGame( int restart ) {
 	if ( trap_Cvar_VariableIntegerValue( "bot_enable" ) ) {
 		BotAIShutdown( restart );
 	}
-
-	G_UnregisterCommands( );
 }
 
 

code/game/g_svcmds.c
@@ -527,15 +527,3 @@ void G_RegisterCommands( void )
 		trap_AddCommand( svcmds[ i ].cmd );
 	}
 }
-
-void G_UnregisterCommands( void )
-{
-	int i;
-
-	for( i = 0; i < numSvCmds; i++ )
-	{
-		if( svcmds[ i ].dedicated && !g_dedicated.integer )
-			continue;
-		trap_RemoveCommand( svcmds[ i ].cmd );
-	}
-}

code/null/null_client.c
@@ -62,18 +62,11 @@ void CL_Disconnect( qboolean showMainMenu ) {
 void CL_MapLoading( void ) {
 }
 
-qboolean CL_GameCommand( void ) {
-  return qfalse;
-}
-
 void CL_KeyEvent (int key, qboolean down, unsigned time) {
 }
 
 qboolean UI_GameCommand( void ) {
 	return qfalse;
-}
-
-void CL_ForwardCommandToServer( const char *string ) {
 }
 
 void CL_ConsolePrint( char *txt ) {

commit 6a2a25126a6ff54cc2db9dea06fc25e28bed05b0

Fix executing UI commands
UI commands were broken by a1d7aeec6088b7c96c366b413e3c15d8f3dc0b4a which requires all commands to be added using trap_AddCommand.
Did some cleanup/refactoring of cgame console command code.

code/cgame/cg_consolecmds.c
@@ -32,6 +32,7 @@ Suite 120, Rockville, Maryland 20850 USA.
 // executed by a key binding
 
 #include "cg_local.h"
+#include "../ui/ui_public.h"
 #ifdef MISSIONPACK
 #include "../ui/ui_shared.h"
 #endif
@@ -557,15 +558,7 @@ void CG_GenerateTracemap(void)
 	BG_GenerateTracemap(cgs.mapname, cg.mapcoordsMins, cg.mapcoordsMaxs, &gen);
 }
 
-#define CMD_INGAME 1
-
-typedef struct {
-	char	*cmd;
-	void	(*function)(void);
-	int		flags;
-} consoleCommand_t;
-
-static consoleCommand_t	commands[] = {
+static consoleCommand_t	cg_commands[] = {
 	{ "testgun", CG_TestGun_f, CMD_INGAME },
 	{ "testmodel", CG_TestModel_f, CMD_INGAME },
 	{ "nextframe", CG_TestModelNextFrame_f, CMD_INGAME },
@@ -588,6 +581,8 @@ static consoleCommand_t	commands[] = {
 	{ "loaddeferred", CG_LoadDeferredPlayers, CMD_INGAME },
 	{ "generateTracemap", CG_GenerateTracemap, CMD_INGAME }
 };
+
+static int cg_numCommands = ARRAY_LEN( cg_commands );
 
 typedef struct {
 	char	*cmd;
@@ -692,6 +687,47 @@ static playerConsoleCommand_t	playerCommands[] = {
 	{ "weapon", CG_Weapon_f, CMD_INGAME }
 };
 
+static int numPlayerCommands = ARRAY_LEN( playerCommands );
+
+/*
+=================
+CG_CheckCmdFlags
+=================
+*/
+static qboolean CG_CheckCmdFlags( const char *cmd, int flags ) {
+	if ( ( flags & CMD_INGAME ) && !cg.snap ) {
+		CG_Printf("Must be in game to use command \"%s\"\n", cmd);
+		return qtrue;
+	}
+
+	if ( ( flags & CMD_MENU ) && cg.connected ) {
+		CG_Printf("Cannot use command \"%s\" while in game\n", cmd);
+		return qtrue;
+	}
+
+	return qfalse;
+}
+
+/*
+=================
+CG_CheckCommands
+=================
+*/
+static qboolean CG_CheckCommands( consoleCommand_t *commands, int numCommands, const char *cmd ) {
+	int i;
+
+	for ( i = 0 ; i < numCommands ; i++ ) {
+		if ( !Q_stricmp( cmd, commands[i].cmd )) {
+			if ( CG_CheckCmdFlags( cmd, commands[i].flags ) ) {
+				return qtrue;
+			}
+			commands[i].function();
+			return qtrue;
+		}
+	}
+	return qfalse;
+}
+
 /*
 =================
 CG_ConsoleCommand
@@ -700,22 +736,24 @@ The string has been tokenized and can be retrieved with
 Cmd_Argc() / Cmd_Argv()
 =================
 */
-qboolean CG_ConsoleCommand( void ) {
+qboolean CG_ConsoleCommand( int realTime ) {
 	char		buffer[BIG_INFO_STRING];
 	const char	*cmd;
 	int		i;
 	int		localPlayerNum;
 	const char	*baseCmd;
 
+	// update UI frame time
+	UI_ConsoleCommand( realTime );
+
 	cmd = CG_Argv(0);
 
 	localPlayerNum = Com_LocalClientForCvarName( cmd );
 	baseCmd = Com_LocalClientBaseCvarName( cmd );
 
-	for ( i = 0 ; i < ARRAY_LEN( playerCommands ) ; i++ ) {
+	for ( i = 0 ; i < numPlayerCommands ; i++ ) {
 		if ( !Q_stricmp( baseCmd, playerCommands[i].cmd )) {
-			if ( ( playerCommands[i].flags & CMD_INGAME ) && !cg.snap ) {
-				CG_Printf("You must be in game to use command \"%s\"\n", cmd);
+			if ( CG_CheckCmdFlags( cmd, playerCommands[i].flags ) ) {
 				return qtrue;
 			}
 			playerCommands[i].function( localPlayerNum );
@@ -724,15 +762,12 @@ qboolean CG_ConsoleCommand( void ) {
 	}
 
 	if ( localPlayerNum == 0 ) {
-		for ( i = 0 ; i < ARRAY_LEN( commands ) ; i++ ) {
-			if ( !Q_stricmp( cmd, commands[i].cmd )) {
-				if ( ( commands[i].flags & CMD_INGAME ) && !cg.snap ) {
-					CG_Printf("You must be in game to use command \"%s\"\n", cmd);
-					return qtrue;
-				}
-				commands[i].function();
-				return qtrue;
-			}
+		if ( CG_CheckCommands( cg_commands, cg_numCommands, cmd ) ) {
+			return qtrue;
+		}
+
+		if ( CG_CheckCommands( ui_commands, ui_numCommands, cmd ) ) {
+			return qtrue;
 		}
 	}
 
@@ -758,11 +793,15 @@ so it can perform tab completion
 void CG_InitConsoleCommands( void ) {
 	int		i, j;
 
-	for ( i = 0 ; i < ARRAY_LEN( commands ) ; i++ ) {
-		trap_AddCommand( commands[i].cmd );
+	for ( i = 0 ; i < cg_numCommands ; i++ ) {
+		trap_AddCommand( cg_commands[i].cmd );
 	}
 
-	for ( i = 0 ; i < ARRAY_LEN( playerCommands ) ; i++ ) {
+	for ( i = 0 ; i < ui_numCommands ; i++ ) {
+		trap_AddCommand( ui_commands[i].cmd );
+	}
+
+	for ( i = 0 ; i < numPlayerCommands ; i++ ) {
 		for ( j = 0; j < CG_MaxSplitView(); j++ ) {
 			trap_AddCommand( Com_LocalClientCvarName( j, playerCommands[i].cmd ) );
 		}

code/cgame/cg_info.c
@@ -31,6 +31,7 @@ Suite 120, Rockville, Maryland 20850 USA.
 // cg_info.c -- display information while data is being loading
 
 #include "cg_local.h"
+#include "../ui/ui_public.h"
 
 #define MAX_LOADING_PLAYER_ICONS	16
 #define MAX_LOADING_ITEM_ICONS		26

code/cgame/cg_local.h
@@ -32,7 +32,6 @@ Suite 120, Rockville, Maryland 20850 USA.
 #include "../renderercommon/tr_types.h"
 #include "../game/bg_misc.h"
 #include "../client/keycodes.h"
-#include "../ui/ui_public.h"
 #include "cg_public.h"
 #include "cg_syscalls.h"
 
@@ -1628,7 +1627,16 @@ void CG_DrawOldTourneyScoreboard( void );
 //
 // cg_consolecmds.c
 //
-qboolean CG_ConsoleCommand( void );
+#define CMD_INGAME	1 // only usable while in-game
+#define CMD_MENU	2 // only usable while at main menu
+
+typedef struct {
+	char	*cmd;
+	void	(*function)(void);
+	int		flags;
+} consoleCommand_t;
+
+qboolean CG_ConsoleCommand( int realTime );
 void CG_InitConsoleCommands( void );
 
 //

code/cgame/cg_main.c
@@ -30,6 +30,7 @@ Suite 120, Rockville, Maryland 20850 USA.
 //
 // cg_main.c -- initialization and primary entry point for cgame
 #include "cg_local.h"
+#include "../ui/ui_public.h"
 
 #ifdef MISSIONPACK_HUD
 #include "../ui/ui_shared.h"
@@ -74,12 +75,7 @@ Q_EXPORT intptr_t vmMain( int command, int arg0, int arg1, int arg2, int arg3, i
 		CG_Shutdown();
 		return 0;
 	case CG_CONSOLE_COMMAND:
-		{
-			qboolean found = UI_ConsoleCommand(arg0);
-			if ( !found )
-				found = CG_ConsoleCommand();
-			return found;
-		}
+		return CG_ConsoleCommand(arg0);
 	case CG_REFRESH:
 		CG_Refresh( arg0, arg1, arg2, arg3, arg4 );
 		return 0;

code/q3_ui/ui_atoms.c
@@ -1025,58 +1025,31 @@ void UI_Cache_f( void ) {
 }
 
 
+consoleCommand_t	ui_commands[] = {
+	{ "levelselect", UI_SPLevelMenu_f, 0 },
+	{ "postgame", UI_SPPostgameMenu_f, CMD_INGAME },
+	{ "ui_cache", UI_Cache_f, 0 },
+	{ "ui_cinematics", UI_CinematicsMenu_f, 0 },
+	{ "ui_teamOrders", UI_TeamOrdersMenu_f, CMD_INGAME },
+	{ "iamacheater", UI_SPUnlock_f, 0 },
+	{ "iamamonkey", UI_SPUnlockMedals_f, 0 }
+};
+
+int ui_numCommands = ARRAY_LEN( ui_commands );
+
 /*
 =================
 UI_ConsoleCommand
+
+update frame time, commands are executed by CG_ConsoleCommand
 =================
 */
-qboolean UI_ConsoleCommand( int realTime ) {
-	const char	*cmd;
-
+void UI_ConsoleCommand( int realTime ) {
 	uis.frametime = realTime - uis.realtime;
 	uis.realtime = realTime;
 
-	cmd = CG_Argv( 0 );
-
 	// ensure minimum menu data is available
 	Menu_Cache();
-
-	if ( Q_stricmp (cmd, "levelselect") == 0 ) {
-		UI_SPLevelMenu_f();
-		return qtrue;
-	}
-
-	if ( Q_stricmp (cmd, "postgame") == 0 ) {
-		UI_SPPostgameMenu_f();
-		return qtrue;
-	}
-
-	if ( Q_stricmp (cmd, "ui_cache") == 0 ) {
-		UI_Cache_f();
-		return qtrue;
-	}
-
-	if ( Q_stricmp (cmd, "ui_cinematics") == 0 ) {
-		UI_CinematicsMenu_f();
-		return qtrue;
-	}
-
-	if ( Q_stricmp (cmd, "ui_teamOrders") == 0 ) {
-		UI_TeamOrdersMenu_f();
-		return qtrue;
-	}
-
-	if ( Q_stricmp (cmd, "iamacheater") == 0 ) {
-		UI_SPUnlock_f();
-		return qtrue;
-	}
-
-	if ( Q_stricmp (cmd, "iamamonkey") == 0 ) {
-		UI_SPUnlockMedals_f();
-		return qtrue;
-	}
-
-	return qfalse;
 }
 
 /*

code/q3_ui/ui_local.h
@@ -32,6 +32,7 @@ Suite 120, Rockville, Maryland 20850 USA.
 #define __UI_LOCAL_H__
 
 #include "../cgame/cg_local.h"
+#include "../ui/ui_public.h"
 
 typedef void (*voidfunc_f)(void);
 

code/ui/ui_atoms.c
@@ -256,75 +256,64 @@ static void UI_CalcPostGameStats( void ) {
 
 }
 
+static void UI_Test_f( void ) {
+	UI_ShowPostGame(qtrue);
+}
+
+static void UI_Load_f( void ) {
+#ifdef MISSIONPACK_HUD
+	if ( cg.connected ) {
+		// if hud scripts are loaded, UI_Load() will break it. So reload hud and ui using loadhud command.
+		trap_Cmd_ExecuteText( EXEC_NOW, "loadhud\n" );
+		return;
+	}
+#endif
+	UI_Load();
+}
+
+static void UI_RemapShader_f( void ) {
+	if (trap_Argc() == 4) {
+		char shader1[MAX_QPATH];
+		char shader2[MAX_QPATH];
+		char shader3[MAX_QPATH];
+
+		Q_strncpyz(shader1, CG_Argv(1), sizeof(shader1));
+		Q_strncpyz(shader2, CG_Argv(2), sizeof(shader2));
+		Q_strncpyz(shader3, CG_Argv(3), sizeof(shader3));
+
+		trap_R_RemapShader(shader1, shader2, shader3);
+	}
+}
+
+consoleCommand_t	ui_commands[] = {
+	//{ "levelselect", UI_SPLevelMenu_f, 0 },
+	{ "postgame", UI_CalcPostGameStats, CMD_INGAME },
+	{ "ui_cache", UI_Cache_f, 0 },
+	//{ "ui_cinematics", UI_CinematicsMenu_f, 0 },
+	//{ "ui_teamOrders", UI_TeamOrdersMenu_f, CMD_INGAME },
+	//{ "iamacheater", UI_SPUnlock_f, 0 },
+	//{ "iamamonkey", UI_SPUnlockMedals_f, 0 },
+	{ "ui_test", UI_Test_f, CMD_INGAME },
+	{ "ui_report", UI_Report, 0 },
+	{ "ui_load", UI_Load_f, 0 },
+	{ "remapShader", UI_RemapShader_f, 0 }
+};
+
+int ui_numCommands = ARRAY_LEN( ui_commands );
 
 /*
 =================
 UI_ConsoleCommand
+
+update frame time, commands are executed by CG_ConsoleCommand
 =================
 */
-qboolean UI_ConsoleCommand( int realTime ) {
-	const char	*cmd;
-
+void UI_ConsoleCommand( int realTime ) {
 	uiInfo.uiDC.frameTime = realTime - uiInfo.uiDC.realTime;
 	uiInfo.uiDC.realTime = realTime;
 
-	cmd = CG_Argv( 0 );
-
 	// ensure minimum menu data is available
 	//Menu_Cache();
-
-	if ( Q_stricmp (cmd, "ui_test") == 0 ) {
-		UI_ShowPostGame(qtrue);
-	}
-
-	if ( Q_stricmp (cmd, "ui_report") == 0 ) {
-		UI_Report();
-		return qtrue;
-	}
-	
-	if ( Q_stricmp (cmd, "ui_load") == 0 ) {
-#ifdef MISSIONPACK_HUD
-		if ( cg.connected ) {
-			// if hud scripts are loaded, UI_Load() will break it. So reload hud and ui using loadhud command.
-			trap_Cmd_ExecuteText( EXEC_NOW, "loadhud\n" );
-			return qtrue;
-		}
-#endif
-		UI_Load();
-		return qtrue;
-	}
-
-	if ( Q_stricmp (cmd, "remapShader") == 0 ) {
-		if (trap_Argc() == 4) {
-			char shader1[MAX_QPATH];
-			char shader2[MAX_QPATH];
-			char shader3[MAX_QPATH];
-			
-			Q_strncpyz(shader1, CG_Argv(1), sizeof(shader1));
-			Q_strncpyz(shader2, CG_Argv(2), sizeof(shader2));
-			Q_strncpyz(shader3, CG_Argv(3), sizeof(shader3));
-			
-			trap_R_RemapShader(shader1, shader2, shader3);
-			return qtrue;
-		}
-	}
-
-	if ( Q_stricmp (cmd, "postgame") == 0 ) {
-		UI_CalcPostGameStats();
-		return qtrue;
-	}
-
-	if ( Q_stricmp (cmd, "ui_cache") == 0 ) {
-		UI_Cache_f();
-		return qtrue;
-	}
-
-	if ( Q_stricmp (cmd, "ui_teamOrders") == 0 ) {
-		//UI_TeamOrdersMenu_f();
-		return qtrue;
-	}
-
-	return qfalse;
 }
 
 qboolean UI_CursorInRect (int x, int y, int width, int height)

code/ui/ui_local.h
@@ -32,6 +32,7 @@ Suite 120, Rockville, Maryland 20850 USA.
 #define __UI_LOCAL_H__
 
 #include "../cgame/cg_local.h"
+#include "ui_public.h"
 #include "ui_shared.h"
 
 // global display context

code/ui/ui_public.h
@@ -33,6 +33,10 @@ Suite 120, Rockville, Maryland 20850 USA.
 
 #include "../cgame/cg_public.h"
 
+// UI commands
+extern consoleCommand_t ui_commands[];
+extern int ui_numCommands;
+
 // UI functions used by cgame
 
 void	UI_Init( qboolean inGameLoad, int maxSplitView );
@@ -47,7 +51,7 @@ qboolean UI_IsFullscreen( void );
 
 void	UI_Refresh( int time );
 void	UI_SetActiveMenu( uiMenuCommand_t menu );
-qboolean UI_ConsoleCommand( int realTime );
+void	UI_ConsoleCommand( int realTime );
 
 void	UI_DrawConnectScreen( qboolean overlay );
 // if !overlay, the background will be drawn, otherwise it will be