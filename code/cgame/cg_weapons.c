2fc22ac5260e930bc2132a24634cc482d7524abd
Zack Middleton <zturtleman@gmail.com>
2013-10-03 09:44:52 -0500

    Don't register chaingun start spin sound each time

--- code/cgame/cg_weapons.c
+++ code/cgame/cg_weapons.c
@@ -1163,7 +1163,7 @@ static float	CG_MachinegunSpinAngle( centity_t *cent ) {
 		cent->pe.barrelSpinning = !!(cent->currentState.eFlags & EF_FIRING);
 #ifdef MISSIONPACK
 		if ( cent->currentState.weapon == WP_CHAINGUN && !cent->pe.barrelSpinning ) {
-			trap_S_StartSound( NULL, cent->currentState.number, CHAN_WEAPON, trap_S_RegisterSound( "sound/weapons/vulcan/wvulwind.wav", qfalse ) );
+			trap_S_StartSound( NULL, cent->currentState.number, CHAN_WEAPON, cgs.media.sfx_chgstart );
 		}
 #endif
 	}
