zturtleman/spearmint

commit 1c1804d3aa1b396e670566c90f99345ca10afb77

Move client console to cgame
Move bind parsing to cgame.
Console text is now scaled up at larger resolutions.
Console now uses version from 'version' cvar, includes platform and compile date.
Improved console word wrapping.
Started making console work with variable width fonts.

code/cgame/cg_main.c
@@ -49,6 +49,7 @@ void CG_Ingame_Init( int serverMessageNum, int serverCommandSequence, int maxSpl
 void CG_Shutdown( void );
 void CG_Refresh( int serverTime, stereoFrame_t stereoView, qboolean demoPlayback, connstate_t state, int realTime );
 static char *CG_VoIPString( int localClientNum );
+void CG_ParseBinding( int key, qboolean down, unsigned time );
 void Message_Key( int key, qboolean down );
 
 
@@ -90,6 +91,16 @@ Q_EXPORT intptr_t vmMain( int command, int arg0, int arg1, int arg2, int arg3, i
 		{
 			int key = arg0;
 			qboolean down = arg1;
+			unsigned time = arg2;
+
+			// console key is hardcoded, so the user can never unbind it
+			if( key == K_CONSOLE || ( key == K_ESCAPE && trap_Key_IsDown( K_SHIFT ) ) )
+			{
+				if ( down ) {
+					Con_ToggleConsole_f();
+				}
+				return 0;
+			}
 
 			if ( key == K_ESCAPE && down && !( trap_Key_GetCatcher( ) & KEYCATCH_UI ) ) {
 				uiClientState_t cls;
@@ -109,9 +120,16 @@ Q_EXPORT intptr_t vmMain( int command, int arg0, int arg1, int arg2, int arg3, i
 				}
 				return 0;
 			}
+
+			if ( key != K_ESCAPE ) {
+				// send the bound action
+				CG_ParseBinding( key, down, time );
+			}
 		}
 
-		if ( trap_Key_GetCatcher( ) & KEYCATCH_MESSAGE ) {
+		if ( trap_Key_GetCatcher( ) & KEYCATCH_CONSOLE ) {
+			Console_Key( arg0, arg1 );
+		} else if ( trap_Key_GetCatcher( ) & KEYCATCH_MESSAGE ) {
 			Message_Key( arg0, arg1 );
 		} else if ( cg.connected && ( trap_Key_GetCatcher( ) & KEYCATCH_CGAME ) ) {
 			CG_KeyEvent(arg0, arg1);
@@ -141,7 +159,10 @@ Q_EXPORT intptr_t vmMain( int command, int arg0, int arg1, int arg2, int arg3, i
 		CG_EventHandling(arg0);
 		return 0;
 	case CG_CONSOLE_TEXT:
-		CG_AddNotifyText();
+		CG_AddNotifyText(arg0, arg1);
+		return 0;
+	case CG_CONSOLE_CLOSE:
+		CG_CloseConsole();
 		return 0;
 	case CG_WANTSBINDKEYS:
 		return UI_WantsBindKeys();
@@ -161,6 +182,10 @@ centity_t			cg_entities[MAX_GENTITIES];
 weaponInfo_t		cg_weapons[MAX_WEAPONS];
 itemInfo_t			cg_items[MAX_ITEMS];
 
+
+vmCvar_t	con_conspeed;
+vmCvar_t	con_autochat;
+vmCvar_t	con_autoclear;
 
 vmCvar_t	cg_railTrailTime;
 vmCvar_t	cg_centertime;
@@ -318,6 +343,10 @@ typedef struct {
 #define RANGE_FLOAT(min,max) min, max, qfalse
 
 static cvarTable_t cgameCvarTable[] = {
+	{ &con_conspeed, "scr_conspeed", "3", 0, RANGE_FLOAT(0.1, 100) },
+	{ &con_autochat, "con_autochat", "0", CVAR_ARCHIVE, RANGE_ALL },
+	{ &con_autoclear, "con_autoclear", "0", CVAR_ARCHIVE, RANGE_ALL },
+
 	{ &cg_ignore, "cg_ignore", "0", 0, RANGE_ALL },	// used for debugging
 	{ &cg_zoomFov, "cg_zoomfov", "22.5", CVAR_ARCHIVE, RANGE_FLOAT(1, 160) },
 	{ &cg_fov, "cg_fov", "90", CVAR_ARCHIVE, RANGE_FLOAT(1, 160) },
@@ -741,13 +770,14 @@ void CG_RemoveNotifyLine( cglc_t *localClient )
 CG_AddNotifyText
 =================
 */
-void CG_AddNotifyText( void ) {
+void CG_AddNotifyText( int realTime, qboolean restoredText ) {
 	char text[ MAX_CONSOLE_TEXT - 1 ];
 	char *buffer;
 	int bufferLen;
 	int lc;
 	cglc_t *localClient;
 	int localClientBits;
+	qboolean skipnotify = qfalse;
 
 	trap_LiteralArgs( text, sizeof ( text ) );
 
@@ -760,17 +790,30 @@ void CG_AddNotifyText( void ) {
 	}
 
 	buffer = text;
-	bufferLen = strlen( buffer );
+
+	// TTimo - prefix for text that shows up in console but not in notify
+	// backported from RTCW
+	if ( !Q_strncmp( buffer, "[skipnotify]", 12 ) ) {
+		skipnotify = qtrue;
+		buffer += 12;
+	}
+
+	CG_ConsolePrint( buffer );
+
+	if ( skipnotify || restoredText || ( trap_Key_GetCatcher() & KEYCATCH_CONSOLE ) ) {
+		return;
+	}
 
 	// [player #] perfix for text that only shows up in notify area for one local client
-	if ( bufferLen > 4 && !Q_strncmp( buffer, "[player ", 8 ) && isdigit(buffer[8]) && buffer[9] == ']' ) {
+	if ( !Q_strncmp( buffer, "[player ", 8 ) && isdigit(buffer[8]) && buffer[9] == ']' ) {
 		localClientBits = 1 << ( atoi( &buffer[8] ) - 1 );
 
 		buffer += 10;
-		bufferLen = strlen( buffer );
 	} else {
 		localClientBits = ~0;
 	}
+
+	bufferLen = strlen( buffer );
 
 	for ( lc = 0; lc < CG_MaxSplitView(); lc++ ) {
 		if ( !( localClientBits & ( 1 << lc ) ) ) {
@@ -2436,6 +2479,7 @@ void CG_Init( qboolean inGameLoad, int maxSplitView ) {
 	// load a few needed things before we do any screen updates
 	cgs.media.charsetShader		= trap_R_RegisterShader( "gfx/2d/bigchars" );
 	cgs.media.whiteShader		= trap_R_RegisterShader( "white" );
+	cgs.media.consoleShader		= trap_R_RegisterShader( "console" );
 
 	// get the rendering configuration from the client system
 	trap_GetGlconfig( &cgs.glconfig );
@@ -2460,6 +2504,13 @@ void CG_Init( qboolean inGameLoad, int maxSplitView ) {
 		cgs.screenYScale = cgs.screenXScale;
 		// no wide screen
 		cgs.screenXBias = 0;
+	}
+
+	CG_ConsoleInit();
+
+	if ( trap_Cvar_VariableIntegerValue( "dedicated" ) ) {
+		trap_Key_SetCatcher( KEYCATCH_CONSOLE );
+		return;
 	}
 
 #ifdef MISSIONPACK_HUD
@@ -2625,7 +2676,7 @@ void CG_Refresh( int serverTime, stereoFrame_t stereoView, qboolean demoPlayback
 		CG_DrawActiveFrame( serverTime, stereoView, demoPlayback );
 	}
 
-	if ( state <= CA_LOADING || (trap_Key_GetCatcher() & KEYCATCH_UI) ) {
+	if ( ( state > CA_DISCONNECTED && state <= CA_LOADING ) || (trap_Key_GetCatcher() & KEYCATCH_UI) ) {
 		if ( ui_stretch.integer ) {
 			CG_SetScreenPlacement( PLACE_STRETCH, PLACE_STRETCH );
 		} else {
@@ -2637,6 +2688,95 @@ void CG_Refresh( int serverTime, stereoFrame_t stereoView, qboolean demoPlayback
 	// connecting clients will show the connection dialog
 	if ( state >= CA_CONNECTING && state < CA_ACTIVE ) {
 		UI_DrawConnectScreen( ( state >= CA_LOADING ) );
+	}
+
+	CG_RunConsole( state );
+}
+
+/*
+===================
+CG_BindUICommand
+
+Returns qtrue if bind command should be executed while user interface is shown
+===================
+*/
+static qboolean CG_BindUICommand( const char *cmd ) {
+	if ( trap_Key_GetCatcher( ) & KEYCATCH_CONSOLE )
+		return qfalse;
+
+	if ( !Q_stricmp( cmd, "toggleconsole" ) )
+		return qtrue;
+	if ( !Q_stricmp( cmd, "togglemenu" ) )
+		return qtrue;
+
+	return qfalse;
+}
+
+/*
+===================
+CG_ParseBinding
+
+Execute the commands in the bind string
+
+key up events only perform actions if the game key binding is
+a button command (leading + sign).  These will be processed even in
+console mode and menu mode, to keep the character from continuing
+an action started before a mode switch.
+
+===================
+*/
+void CG_ParseBinding( int key, qboolean down, unsigned time )
+{
+	char buf[ MAX_STRING_CHARS ], *p = buf, *end;
+	qboolean allCommands, allowUpCmds;
+	uiClientState_t cls;
+
+	trap_GetClientState( &cls );
+
+	if( cls.connState == CA_DISCONNECTED && trap_Key_GetCatcher( ) == 0 )
+		return;
+
+	trap_Key_GetBindingBuf( key, buf, sizeof ( buf ) );
+
+	if( !buf[0] )
+		return;
+
+	// run all bind commands if console, ui, etc aren't reading keys
+	allCommands = ( trap_Key_GetCatcher( ) == 0 );
+
+	// allow button up commands if in game even if key catcher is set
+	allowUpCmds = ( cls.connState != CA_DISCONNECTED );
+
+	while( 1 )
+	{
+		while( isspace( *p ) )
+			p++;
+		end = strchr( p, ';' );
+		if( end )
+			*end = '\0';
+		if( *p == '+' )
+		{
+			// button commands add keynum and time as parameters
+			// so that multiple sources can be discriminated and
+			// subframe corrected
+			if ( allCommands || ( allowUpCmds && !down ) ) {
+				char cmd[1024];
+				Com_sprintf( cmd, sizeof( cmd ), "%c%s %d %d\n",
+					( down ) ? '+' : '-', p + 1, key, time );
+				trap_Cmd_ExecuteText( EXEC_APPEND, cmd );
+			}
+		}
+		else if( down )
+		{
+			// normal commands only execute on key press
+			if ( allCommands || CG_BindUICommand( p ) ) {
+				trap_Cmd_ExecuteText( EXEC_APPEND, p );
+				trap_Cmd_ExecuteText( EXEC_APPEND, "\n" );
+			}
+		}
+		if( !end )
+			break;
+		p = end + 1;
 	}
 }
 

Makefile
@@ -2274,6 +2274,7 @@ Q3CGOBJ_ = \
   $(B)/$(BASEGAME)/cgame/bg_lib.o \
   $(B)/$(BASEGAME)/cgame/bg_tracemap.o \
   $(B)/$(BASEGAME)/cgame/cg_atmospheric.o \
+  $(B)/$(BASEGAME)/cgame/cg_console.o \
   $(B)/$(BASEGAME)/cgame/cg_consolecmds.o \
   $(B)/$(BASEGAME)/cgame/cg_newdraw.o \
   $(B)/$(BASEGAME)/cgame/cg_draw.o \
@@ -2366,6 +2367,7 @@ MPCGOBJ_ = \
   $(B)/$(MISSIONPACK)/cgame/bg_lib.o \
   $(B)/$(MISSIONPACK)/cgame/bg_tracemap.o \
   $(B)/$(MISSIONPACK)/cgame/cg_atmospheric.o \
+  $(B)/$(MISSIONPACK)/cgame/cg_console.o \
   $(B)/$(MISSIONPACK)/cgame/cg_consolecmds.o \
   $(B)/$(MISSIONPACK)/cgame/cg_newdraw.o \
   $(B)/$(MISSIONPACK)/cgame/cg_draw.o \

code/cgame/cg_console.c
@@ -1,0 +1,738 @@
+/*
+===========================================================================
+Copyright (C) 1999-2010 id Software LLC, a ZeniMax Media company.
+
+This file is part of Spearmint Source Code.
+
+Spearmint Source Code is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 3 of the License,
+or (at your option) any later version.
+
+Spearmint Source Code is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Spearmint Source Code.  If not, see <http://www.gnu.org/licenses/>.
+
+In addition, Spearmint Source Code is also subject to certain additional terms.
+You should have received a copy of these additional terms immediately following
+the terms and conditions of the GNU General Public License.  If not, please
+request a copy in writing from id Software at the address below.
+
+If you have questions concerning this license or the applicable additional
+terms, you may contact in writing id Software LLC, c/o ZeniMax Media Inc.,
+Suite 120, Rockville, Maryland 20850 USA.
+===========================================================================
+*/
+//
+#include "cg_local.h"
+
+//#define		CON_TEXTSIZE	32768*2
+#define		CON_MAXLINES	512
+#define		CON_LINELENGTH	128
+typedef struct {
+	qboolean	initialized;
+
+	char		lines[CON_MAXLINES][CON_LINELENGTH];
+
+	int		current;		// line where next message will be printed
+	int		display;		// bottom of console displays this line
+	int		x;			// offset in current line for next print
+
+	int		topline; // 0 <= topline < CON_MAXLINES
+
+	float	displayFrac;	// aproaches finalFrac at scr_conspeed
+	float	finalFrac;		// 0.0 to 1.0 lines of console to display
+
+	char		version[80];
+
+	int		sideMargin;
+	int		screenFakeWidth; // width in fake 640x480, it can be more than 640
+} console_t;
+
+console_t	con;
+
+#define	DEFAULT_CONSOLE_WIDTH	78
+
+#define		COMMAND_HISTORY		32
+
+mfield_t	historyEditLines[COMMAND_HISTORY];
+
+int			nextHistoryLine;		// the last line in the history buffer, not masked
+int			historyLine;	// the line being displayed from history buffer
+							// will be <= nextHistoryLine
+
+mfield_t		g_consoleField;
+int			g_console_field_width = DEFAULT_CONSOLE_WIDTH;
+
+void CG_LoadConsoleHistory( void );
+void CG_SaveConsoleHistory( void );
+
+/*
+================
+Con_ClearConsole_f
+================
+*/
+void Con_ClearConsole_f (void) {
+	int i;
+
+	for ( i = 0; i < CON_MAXLINES; i++ ) {
+		con.lines[i][0] = '\0';
+	}
+
+	con.current = con.display = con.topline = con.x = 0;
+}
+
+/*
+================
+Con_ToggleConsole_f
+================
+*/
+void Con_ToggleConsole_f (void) {
+	uiClientState_t	cls;
+
+	trap_GetClientState( &cls );
+
+	// Can't toggle the console when it's the only thing available
+	if ( cls.connState == CA_DISCONNECTED && trap_Key_GetCatcher( ) == KEYCATCH_CONSOLE ) {
+		return;
+	}
+
+	if ( con_autoclear.integer ) {
+		MField_Clear( &g_consoleField );
+	}
+
+	g_consoleField.widthInChars = g_console_field_width;
+
+	trap_Key_SetCatcher( trap_Key_GetCatcher( ) ^ KEYCATCH_CONSOLE );
+}
+
+/*
+================
+Con_LineFeed
+================
+*/
+void Con_LineFeed( void ) {
+	if ( con.display == con.current ) {
+		con.display++;
+	}
+	con.current++;
+	con.x = 0;
+	con.lines[con.current % CON_MAXLINES][0] = '\0';
+
+	if ( con.current < CON_MAXLINES ) {
+		con.topline = con.current;
+	}
+}
+
+/*
+================
+CG_ConsolePrint
+================
+*/
+void CG_ConsolePrint( const char *p ) {
+	int lineDrawLen, wordDrawLen, charDrawLen;
+	int wordLen;
+	int i;
+
+	while ( *p != '\0' ) {
+		if ( *p == '\r' ) {
+			con.x = 0;
+			p++;
+			continue;
+		} else if ( *p == '\n' ) {
+			Con_LineFeed();
+			p++;
+			continue;
+		}
+
+		lineDrawLen = CG_DrawStrlen( con.lines[con.current % CON_MAXLINES] ) * SMALLCHAR_WIDTH;
+		wordDrawLen = charDrawLen = 0;
+
+		for ( i = 0; i < CON_LINELENGTH; i++ ) {
+			if ( Q_IsColorString( &p[i] ) ) {
+				continue;
+			}
+
+			if ( p[i] == '\0' || p[i] == '\r' || p[i] == '\n' || p[i] == ' ' ) {
+				break;
+			}
+
+			// break string at slashes, but don't leave them trailing at end of line (stick to beginning of next word)
+			if ( i > 0 && ( p[i] == '/' || p[i] == '\\' ) ) {
+				break;
+			}
+
+			charDrawLen = CG_DrawStrlenEx( &p[i], 1 ) * SMALLCHAR_WIDTH;
+
+			// make sure the word will fit on screen, even if it needs a whole line to do so.
+			if ( wordDrawLen + charDrawLen > con.screenFakeWidth - ( con.sideMargin * 2 ) ) {
+				break;
+			}
+
+			wordDrawLen += charDrawLen;
+		}
+
+		// if *p is a word separator or word is too long, just copy one char at a time
+		if ( i == 0 || i == CON_LINELENGTH ) {
+			wordLen = 1;
+		} else {
+			wordLen = i;
+		}
+
+		if ( con.x + wordLen > CON_LINELENGTH ) {
+			Con_LineFeed();
+		} else  if ( lineDrawLen + wordDrawLen > con.screenFakeWidth - ( con.sideMargin * 2 ) ) {
+			Con_LineFeed();
+		}
+
+		Q_strncpyz( &con.lines[con.current % CON_MAXLINES][con.x], p, wordLen+1 );
+		con.x += wordLen;
+		p += wordLen;
+	}
+}
+
+
+/*
+==============================================================================
+
+DRAWING
+
+==============================================================================
+*/
+
+
+/*
+================
+Con_DrawInput
+
+Draw the editline after a ] prompt
+================
+*/
+void Con_DrawInput ( connstate_t state, int lines ) {
+	int		y;
+
+	if ( state != CA_DISCONNECTED && !(trap_Key_GetCatcher( ) & KEYCATCH_CONSOLE ) ) {
+		return;
+	}
+
+	y = lines - ( SMALLCHAR_HEIGHT * 2 );
+
+	CG_DrawSmallStringColor( con.sideMargin, y, "]", g_color_table[ColorIndex(COLOR_WHITE)] );
+
+	MField_Draw( &g_consoleField, 2 * SMALLCHAR_WIDTH, y, SMALLCHAR_WIDTH, SMALLCHAR_HEIGHT, g_color_table[ColorIndex(COLOR_WHITE)] );
+}
+
+/*
+================
+Con_DrawSolidConsole
+
+Draws the console with the solid background
+================
+*/
+void Con_DrawSolidConsole( connstate_t state, float frac ) {
+	int				i;
+	int				x, y;
+	int				rows;
+	char			*text;
+	int				row;
+	int				lines;
+	vec4_t			color;
+
+	lines = SCREEN_HEIGHT * frac;
+	if (lines <= 0)
+		return;
+
+	if (lines > SCREEN_HEIGHT )
+		lines = SCREEN_HEIGHT;
+
+	CG_SetScreenPlacement( PLACE_STRETCH, PLACE_STRETCH );
+
+	// draw the background
+	y = frac * SCREEN_HEIGHT;
+	if ( y < 1 ) {
+		y = 0;
+	}
+	else {
+		CG_DrawPic( 0, 0, SCREEN_WIDTH, y, cgs.media.consoleShader );
+	}
+
+	color[0] = 1;
+	color[1] = 0;
+	color[2] = 0;
+	color[3] = 1;
+	CG_FillRect( 0, y, SCREEN_WIDTH, 2, color );
+
+	CG_SetScreenPlacement( PLACE_RIGHT, PLACE_TOP );
+
+	// draw the version number
+	CG_DrawSmallStringColor( SCREEN_WIDTH - CG_DrawStrlen( con.version ) * SMALLCHAR_WIDTH,
+			lines - SMALLCHAR_HEIGHT, con.version, color );
+
+	CG_SetScreenPlacement( PLACE_LEFT, PLACE_TOP );
+
+	// draw the text
+	rows = (lines-SMALLCHAR_HEIGHT)/SMALLCHAR_HEIGHT;		// rows of text to draw
+
+	y = lines - (SMALLCHAR_HEIGHT*3);
+
+	// draw from the bottom up
+	if (con.display != con.current)
+	{
+		int linewidth = con.screenFakeWidth / SMALLCHAR_WIDTH;
+		// draw arrows to show the buffer is backscrolled
+		trap_R_SetColor( color );
+		for (x=0 ; x<linewidth ; x+=4)
+			CG_DrawChar( (x+1)*SMALLCHAR_WIDTH, y, SMALLCHAR_WIDTH, SMALLCHAR_HEIGHT, '^' );
+		y -= SMALLCHAR_HEIGHT;
+		rows--;
+		trap_R_SetColor( NULL );
+	}
+	
+	row = con.display;
+
+	if ( con.x == 0 ) {
+		row--;
+	}
+
+	for (i=0 ; i<rows ; i++, y -= SMALLCHAR_HEIGHT, row--)
+	{
+		if (row < 0)
+			break;
+		if (con.current - row >= CON_MAXLINES) {
+			// past scrollback wrap point
+			continue;
+		}
+
+		text = con.lines[row % CON_MAXLINES];
+		CG_DrawSmallString( con.sideMargin, y, text, 1.0f );
+	}
+
+	// draw the input prompt, user text, and cursor if desired
+	Con_DrawInput ( state, lines );
+}
+
+
+
+/*
+==================
+Con_DrawConsole
+==================
+*/
+void Con_DrawConsole( connstate_t state ) {
+	// if disconnected, render console full screen
+	if ( state == CA_DISCONNECTED ) {
+		if ( !( trap_Key_GetCatcher( ) & KEYCATCH_UI) ) {
+			Con_DrawSolidConsole( state, 1.0f );
+			return;
+		}
+	}
+
+	if ( con.displayFrac ) {
+		Con_DrawSolidConsole( state, con.displayFrac );
+	}
+}
+
+//================================================================
+
+/*
+==================
+CG_RunConsole
+
+Scroll it up or down and draw it
+==================
+*/
+void CG_RunConsole( connstate_t state ) {
+	// decide on the destination height of the console
+	if ( trap_Key_GetCatcher( ) & KEYCATCH_CONSOLE )
+		con.finalFrac = 0.5;		// half screen
+	else
+		con.finalFrac = 0;				// none visible
+	
+	// scroll towards the destination height
+	if (con.finalFrac < con.displayFrac)
+	{
+		con.displayFrac -= con_conspeed.value*cg.realFrameTime*0.001;
+		if (con.finalFrac > con.displayFrac)
+			con.displayFrac = con.finalFrac;
+
+	}
+	else if (con.finalFrac > con.displayFrac)
+	{
+		con.displayFrac += con_conspeed.value*cg.realFrameTime*0.001;
+		if (con.finalFrac < con.displayFrac)
+			con.displayFrac = con.finalFrac;
+	}
+
+	Con_DrawConsole( state );
+}
+
+
+void Con_PageUp( void ) {
+	con.display -= 2;
+	if ( con.current - con.display >= con.topline ) {
+		con.display = con.current - con.topline + 1;
+	}
+}
+
+void Con_PageDown( void ) {
+	con.display += 2;
+	if (con.display > con.current) {
+		con.display = con.current;
+	}
+}
+
+void Con_Top( void ) {
+	con.display = con.topline;
+	if ( con.current - con.display >= con.topline ) {
+		con.display = con.current - con.topline + 1;
+	}
+}
+
+void Con_Bottom( void ) {
+	con.display = con.current;
+}
+
+
+void CG_CloseConsole( void ) {
+	MField_Clear( &g_consoleField );
+	trap_Key_SetCatcher( trap_Key_GetCatcher( ) & ~KEYCATCH_CONSOLE );
+	con.finalFrac = 0;				// none visible
+	con.displayFrac = 0;
+}
+
+/*
+=============================================================================
+
+CONSOLE LINE EDITING
+
+==============================================================================
+*/
+
+/*
+====================
+Console_Key
+
+Handles history and console scrollback
+====================
+*/
+void Console_Key ( int key, qboolean down ) {
+
+	if ( !down ) {
+		return;
+	}
+
+	if ( key & K_CHAR_FLAG ) {
+		key &= ~K_CHAR_FLAG;
+		MField_CharEvent( &g_consoleField, key );
+		return;
+	}
+
+	// ctrl-L clears screen
+	if ( key == 'l' && trap_Key_IsDown( K_CTRL ) ) {
+		trap_Cmd_ExecuteText( EXEC_APPEND, "clear\n" );
+		return;
+	}
+
+	// enter finishes the line
+	if ( key == K_ENTER || key == K_KP_ENTER ) {
+		uiClientState_t cls;
+
+		trap_GetClientState( &cls );
+
+		// if not in the game explicitly prepend a slash if needed
+		if ( cls.connState != CA_ACTIVE && con_autochat.integer &&
+				g_consoleField.buffer[0] &&
+				g_consoleField.buffer[0] != '\\' &&
+				g_consoleField.buffer[0] != '/' ) {
+			char	temp[MAX_EDIT_LINE-1];
+
+			Q_strncpyz( temp, g_consoleField.buffer, sizeof( temp ) );
+			Com_sprintf( g_consoleField.buffer, sizeof( g_consoleField.buffer ), "\\%s", temp );
+			g_consoleField.cursor++;
+		}
+
+		Com_Printf ( "]%s\n", g_consoleField.buffer );
+
+		// leading slash is an explicit command
+		if ( g_consoleField.buffer[0] == '\\' || g_consoleField.buffer[0] == '/' ) {
+			trap_Cmd_ExecuteText( EXEC_APPEND, g_consoleField.buffer+1 );	// valid command
+			trap_Cmd_ExecuteText( EXEC_APPEND, "\n" );
+		} else {
+			// other text will be chat messages
+			if ( !g_consoleField.buffer[0] ) {
+				return;	// empty lines just scroll the console without adding to history
+			} else {
+				if ( con_autochat.integer ) {
+					trap_Cmd_ExecuteText( EXEC_APPEND, "cmd say " );
+				}
+				trap_Cmd_ExecuteText( EXEC_APPEND, g_consoleField.buffer );
+				trap_Cmd_ExecuteText( EXEC_APPEND, "\n" );
+			}
+		}
+
+		// copy line to history buffer
+		historyEditLines[nextHistoryLine % COMMAND_HISTORY] = g_consoleField;
+		nextHistoryLine++;
+		historyLine = nextHistoryLine;
+
+		MField_Clear( &g_consoleField );
+
+		g_consoleField.widthInChars = g_console_field_width;
+
+		CG_SaveConsoleHistory( );
+
+		if ( cls.connState == CA_DISCONNECTED ) {
+			trap_UpdateScreen();	// force an update, because the command
+		}							// may take some time
+		return;
+	}
+
+	// command completion
+
+	if (key == K_TAB) {
+		char newbuf[MAX_EDIT_LINE];
+
+		trap_Cmd_AutoComplete( g_consoleField.buffer, newbuf, sizeof ( newbuf ) );
+
+		if ( strcmp( newbuf, g_consoleField.buffer ) != 0 ) {
+			Q_strncpyz( g_consoleField.buffer, newbuf, sizeof ( g_consoleField.buffer ) );
+			g_consoleField.cursor = strlen( g_consoleField.buffer );
+		}
+		return;
+	}
+
+	// command history (ctrl-p ctrl-n for unix style)
+
+	if ( ( key == K_MWHEELUP && trap_Key_IsDown( K_SHIFT ) ) || ( key == K_UPARROW ) || ( key == K_KP_UPARROW ) ||
+		 ( ( tolower(key) == 'p' ) && trap_Key_IsDown( K_CTRL ) ) ) {
+		if ( nextHistoryLine - historyLine < COMMAND_HISTORY 
+			&& historyLine > 0 ) {
+			historyLine--;
+		}
+		g_consoleField = historyEditLines[ historyLine % COMMAND_HISTORY ];
+		return;
+	}
+
+	if ( ( key == K_MWHEELDOWN && trap_Key_IsDown( K_SHIFT ) ) || ( key == K_DOWNARROW ) || ( key == K_KP_DOWNARROW ) ||
+		 ( ( tolower(key) == 'n' ) && trap_Key_IsDown( K_CTRL ) ) ) {
+		historyLine++;
+		if (historyLine >= nextHistoryLine) {
+			historyLine = nextHistoryLine;
+			MField_Clear( &g_consoleField );
+			g_consoleField.widthInChars = g_console_field_width;
+			return;
+		}
+		g_consoleField = historyEditLines[ historyLine % COMMAND_HISTORY ];
+		return;
+	}
+
+	// console scrolling
+	if ( key == K_PGUP || key == K_KP_PGUP ) {
+		Con_PageUp();
+		return;
+	}
+
+	if ( key == K_PGDN || key == K_KP_PGDN ) {
+		Con_PageDown();
+		return;
+	}
+
+	if ( key == K_MWHEELUP) {	//----(SA)	added some mousewheel functionality to the console
+		Con_PageUp();
+		if ( trap_Key_IsDown( K_CTRL ) ) {	// hold <ctrl> to accelerate scrolling
+			Con_PageUp();
+			Con_PageUp();
+		}
+		return;
+	}
+
+	if ( key == K_MWHEELDOWN) {	//----(SA)	added some mousewheel functionality to the console
+		Con_PageDown();
+		if ( trap_Key_IsDown( K_CTRL ) ) {	// hold <ctrl> to accelerate scrolling
+			Con_PageDown();
+			Con_PageDown();
+		}
+		return;
+	}
+
+	// ctrl-home = top of console
+	if ( ( key == K_HOME || key == K_KP_HOME ) && trap_Key_IsDown( K_CTRL ) ) {
+		Con_Top();
+		return;
+	}
+
+	// ctrl-end = bottom of console
+	if ( ( key == K_END || key == K_KP_END ) && trap_Key_IsDown( K_CTRL ) ) {
+		Con_Bottom();
+		return;
+	}
+
+	// pass to the normal editline routine
+	MField_KeyDownEvent( &g_consoleField, key );
+}
+
+
+// This must not exceed MAX_CMD_LINE
+#define			MAX_CONSOLE_SAVE_BUFFER	1024
+#define			CONSOLE_HISTORY_FILE    "consolehistory.dat"
+static char	consoleSaveBuffer[ MAX_CONSOLE_SAVE_BUFFER ];
+static int	consoleSaveBufferSize = 0;
+
+/*
+================
+CG_LoadConsoleHistory
+
+Load the console history from cl_consoleHistory
+================
+*/
+void CG_LoadConsoleHistory( void )
+{
+	char					*token, *text_p;
+	int						i, numChars, numLines = 0;
+	fileHandle_t	f;
+
+	consoleSaveBufferSize = trap_FS_FOpenFile( CONSOLE_HISTORY_FILE, &f, FS_READ );
+	if( !f )
+	{
+		Com_Printf( "Couldn't read %s.\n", CONSOLE_HISTORY_FILE );
+		return;
+	}
+
+	if( consoleSaveBufferSize <= MAX_CONSOLE_SAVE_BUFFER &&
+			trap_FS_Read( consoleSaveBuffer, consoleSaveBufferSize, f ) == consoleSaveBufferSize )
+	{
+		text_p = consoleSaveBuffer;
+
+		for( i = COMMAND_HISTORY - 1; i >= 0; i-- )
+		{
+			if( !*( token = COM_Parse( &text_p ) ) )
+				break;
+
+			historyEditLines[ i ].cursor = atoi( token );
+
+			if( !*( token = COM_Parse( &text_p ) ) )
+				break;
+
+			historyEditLines[ i ].scroll = atoi( token );
+
+			if( !*( token = COM_Parse( &text_p ) ) )
+				break;
+
+			numChars = atoi( token );
+			text_p++;
+			if( numChars > ( strlen( consoleSaveBuffer ) -	( text_p - consoleSaveBuffer ) ) )
+			{
+				Com_DPrintf( S_COLOR_YELLOW "WARNING: probable corrupt history\n" );
+				break;
+			}
+			Com_Memcpy( historyEditLines[ i ].buffer,
+					text_p, numChars );
+			historyEditLines[ i ].buffer[ numChars ] = '\0';
+			text_p += numChars;
+
+			numLines++;
+		}
+
+		memmove( &historyEditLines[ 0 ], &historyEditLines[ i + 1 ],
+				numLines * sizeof( mfield_t ) );
+		for( i = numLines; i < COMMAND_HISTORY; i++ )
+			MField_Clear( &historyEditLines[ i ] );
+
+		historyLine = nextHistoryLine = numLines;
+	}
+	else
+		Com_Printf( "Couldn't read %s.\n", CONSOLE_HISTORY_FILE );
+
+	trap_FS_FCloseFile( f );
+}
+
+/*
+================
+CG_SaveConsoleHistory
+
+Save the console history into the cvar cl_consoleHistory
+so that it persists across invocations of q3
+================
+*/
+void CG_SaveConsoleHistory( void )
+{
+	int						i;
+	int						lineLength, saveBufferLength, additionalLength;
+	fileHandle_t	f;
+
+	consoleSaveBuffer[ 0 ] = '\0';
+
+	i = ( nextHistoryLine - 1 ) % COMMAND_HISTORY;
+	do
+	{
+		if( historyEditLines[ i ].buffer[ 0 ] )
+		{
+			lineLength = strlen( historyEditLines[ i ].buffer );
+			saveBufferLength = strlen( consoleSaveBuffer );
+
+			//ICK
+			additionalLength = lineLength + strlen( "999 999 999  " );
+
+			if( saveBufferLength + additionalLength < MAX_CONSOLE_SAVE_BUFFER )
+			{
+				Q_strcat( consoleSaveBuffer, MAX_CONSOLE_SAVE_BUFFER,
+						va( "%d %d %d %s ",
+						historyEditLines[ i ].cursor,
+						historyEditLines[ i ].scroll,
+						lineLength,
+						historyEditLines[ i ].buffer ) );
+			}
+			else
+				break;
+		}
+		i = ( i - 1 + COMMAND_HISTORY ) % COMMAND_HISTORY;
+	}
+	while( i != ( nextHistoryLine - 1 ) % COMMAND_HISTORY );
+
+	consoleSaveBufferSize = strlen( consoleSaveBuffer );
+
+	trap_FS_FOpenFile( CONSOLE_HISTORY_FILE, &f, FS_WRITE );
+	if( !f )
+	{
+		Com_Printf( "Couldn't write %s.\n", CONSOLE_HISTORY_FILE );
+		return;
+	}
+
+	if( trap_FS_Write( consoleSaveBuffer, consoleSaveBufferSize, f ) < consoleSaveBufferSize )
+		Com_Printf( "Couldn't write %s.\n", CONSOLE_HISTORY_FILE );
+
+	trap_FS_FCloseFile( f );
+}
+
+
+/*
+================
+CG_ConsoleInit
+================
+*/
+void CG_ConsoleInit( void ) {
+	int i;
+
+	trap_Cvar_VariableStringBuffer( "version", con.version, sizeof ( con.version ) );
+
+	con.sideMargin = SMALLCHAR_WIDTH;
+	// fit across whole screen inside of a 640x480 box
+	con.screenFakeWidth = cgs.glconfig.vidWidth / cgs.screenXScale;
+
+	//g_console_field_width = con.screenFakeWidth / SMALLCHAR_WIDTH - 2;
+
+	MField_Clear( &g_consoleField );
+	g_consoleField.widthInChars = g_console_field_width;
+
+	for ( i = 0 ; i < COMMAND_HISTORY ; i++ ) {
+		MField_Clear( &historyEditLines[i] );
+		historyEditLines[i].widthInChars = g_console_field_width;
+	}
+
+	CG_LoadConsoleHistory();
+}
+

code/cgame/cg_consolecmds.c
@@ -749,7 +749,9 @@ static consoleCommand_t	cg_commands[] = {
 	{ "messageMode", CG_MessageMode_f },
 	{ "messageMode2", CG_MessageMode2_f },
 	{ "messageMode3", CG_MessageMode3_f },
-	{ "messageMode4", CG_MessageMode4_f }
+	{ "messageMode4", CG_MessageMode4_f },
+	{ "clear", Con_ClearConsole_f },
+	{ "toggleconsole", Con_ToggleConsole_f }
 };
 
 static int cg_numCommands = ARRAY_LEN( cg_commands );

code/cgame/cg_drawtools.c
@@ -465,12 +465,12 @@ void CG_DrawSmallStringColor( int x, int y, const char *s, vec4_t color ) {
 
 /*
 =================
-CG_DrawStrlen
+CG_DrawStrlenEx
 
 Returns character count, skiping color escape codes
 =================
 */
-int CG_DrawStrlen( const char *str ) {
+int CG_DrawStrlenEx( const char *str, int maxchars ) {
 	const char *s = str;
 	int count = 0;
 
@@ -481,9 +481,24 @@ int CG_DrawStrlen( const char *str ) {
 			count++;
 			s++;
 		}
+
+		if ( maxchars > 0 && s - str >= maxchars ) {
+			break;
+		}
 	}
 
 	return count;
+}
+
+/*
+=================
+CG_DrawStrlen
+
+Returns character count, skiping color escape codes
+=================
+*/
+int CG_DrawStrlen( const char *str ) {
+	return CG_DrawStrlenEx( str, 0 );
 }
 
 /*

commit 2bbf90d2c01340daaee83b85122f9f5c2267f3a0

Fix UI getting key events it should not
trap_Key_SetCatcher( x ) causes key up events for all helds keys.
Setting KEYCATCH_UI caused UI get escape key up event, which caused
Team Arena to call UI_ForceMenuOff() in the middle of UI_EnterMenu.

Caused by 1c1804d3aa1b396e670566c90f99345ca10afb77.
It made CG_KEY_EVENT be called even if no key catcher is set.

code/cgame/cg_main.c
@@ -133,7 +133,7 @@ Q_EXPORT intptr_t vmMain( int command, int arg0, int arg1, int arg2, int arg3, i
 			Message_Key( arg0, arg1 );
 		} else if ( cg.connected && ( trap_Key_GetCatcher( ) & KEYCATCH_CGAME ) ) {
 			CG_KeyEvent(arg0, arg1);
-		} else {
+		} else if ( trap_Key_GetCatcher( ) & KEYCATCH_UI ) {
 			UI_KeyEvent(arg0, arg1);
 		}
 		return 0;