e9b512cb0486d63c682bd6a15e53b7df9aaa7a32
Zack Middleton <zturtleman@gmail.com>
2014-01-29 17:10:11 -0600

    Fix touching cgame QVM

--- code/server/sv_init.c
+++ code/server/sv_init.c
@@ -453,7 +453,7 @@ static void SV_TouchCGame(void) {
 	fileHandle_t	f;
 	char filename[MAX_QPATH];
 
-	Com_sprintf( filename, sizeof(filename), "vm/%s.qvm", "cgame" );
+	Com_sprintf( filename, sizeof(filename), "vm/%s.qvm", "mint-cgame" );
 	FS_FOpenFileRead( filename, &f, qfalse );
 	if ( f ) {
 		FS_FCloseFile( f );
