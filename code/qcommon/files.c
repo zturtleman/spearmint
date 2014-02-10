5194fec7175abb284150694fdb8627dec7f80112
Zack Middleton <zturtleman@gmail.com>
2014-02-09 21:34:53 -0600

    spearmint: Update FS_IsDemoExt for new extension format
    DEMOEXT was changed to "mintdemo" from q3's "dm_" followed by protocol.

--- code/qcommon/files.c
+++ code/qcommon/files.c
@@ -1094,30 +1094,7 @@ Return qtrue if filename has a demo extension
 
 qboolean FS_IsDemoExt(const char *filename, int namelen)
 {
-	char *ext_test;
-	int index, protocol;
-
-	ext_test = strrchr(filename, '.');
-	if(ext_test && !Q_stricmpn(ext_test + 1, DEMOEXT, ARRAY_LEN(DEMOEXT) - 1))
-	{
-		protocol = atoi(ext_test + ARRAY_LEN(DEMOEXT));
-
-		if(protocol == com_protocol->integer)
-			return qtrue;
-
-#ifdef LEGACY_PROTOCOL
-		if(protocol == com_legacyprotocol->integer)
-			return qtrue;
-#endif
-
-		for(index = 0; demo_protocols[index]; index++)
-		{
-			if(demo_protocols[index] == protocol)
-			return qtrue;
-		}
-	}
-
-	return qfalse;
+	return FS_IsExt( filename, "." DEMOEXT, namelen );
 }
 
 /*
