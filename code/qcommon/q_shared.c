zturtleman/spearmint

commit cef4cd1e60a214b573b51e36fd1942effe696de6

spearmint: Skip data after shader directive until brace or end of line
This allows adding new aruments to existing directives in the future without breaking parsing with past Spearmint versions (starting now).

Each directive should be on a separate line. Q3 will allow 'detail map path/to/image', Spearmint will skip after detail until end of line or finds a brace.

Note: q3map2 skips unknown general directives (q3/spearmint does not) and rest of line for known general directives. q3map2 does not skip rest of line in stage directives though.

code/qcommon/q_shared.c
@@ -490,7 +490,8 @@ char *COM_ParseExt2( char **data_p, qboolean allowLineBreaks, char delimiter )
 		}
 		if ( hasNewLines && !allowLineBreaks )
 		{
-			*data_p = data;
+			// ZTM: Don't move the pointer so that calling SkipRestOfLine afterwards works as expected
+			//*data_p = data;
 			return com_token;
 		}
 
@@ -626,6 +627,25 @@ void SkipRestOfLine ( char **data ) {
 
 	p = *data;
 	while ( (c = *p++) != 0 ) {
+		if ( c == '\n' ) {
+			com_lines++;
+			break;
+		}
+	}
+
+	*data = p;
+}
+
+void SkipRestOfLineUntilBrace ( char **data ) {
+	char	*p;
+	int		c;
+
+	p = *data;
+	while ( (c = *p++) != 0 ) {
+		if ( c == '{' || c == '}' ) {
+			*p--;
+			break;
+		}
 		if ( c == '\n' ) {
 			com_lines++;
 			break;

code/qcommon/q_shared.h
@@ -864,6 +864,7 @@ void	COM_MatchToken( char**buf_p, char *match );
 
 qboolean SkipBracedSection (char **program, int depth);
 void SkipRestOfLine ( char **data );
+void SkipRestOfLineUntilBrace ( char **data );
 
 void Parse1DMatrix (char **buf_p, int x, float *m);
 void Parse2DMatrix (char **buf_p, int y, int x, float *m);

code/renderergl1/tr_shader.c
@@ -913,11 +913,18 @@ static qboolean ParseStage( shaderStage_t *stage, char **text, int *ifIndent )
 	char *token;
 	int depthMaskBits = GLS_DEPTHMASK_TRUE, blendSrcBits = 0, blendDstBits = 0, atestBits = 0, depthFuncBits = 0;
 	qboolean depthMaskExplicit = qfalse;
+	qboolean skipRestOfLine = qfalse;
 
 	stage->active = qtrue;
 
 	while ( 1 )
 	{
+		// skip unused data for known parameters, but not the initial {
+		if ( skipRestOfLine ) {
+			SkipRestOfLineUntilBrace( text );
+		}
+		skipRestOfLine = qtrue;
+
 		token = COM_ParseExt( text, qtrue );
 		if ( !token[0] )
 		{
@@ -1884,6 +1891,7 @@ static qboolean ParseShader( char **text )
 	char *token;
 	int s;
 	int ifIndent = 0;
+	qboolean skipRestOfLine = qfalse;
 
 	s = 0;
 
@@ -1896,6 +1904,12 @@ static qboolean ParseShader( char **text )
 
 	while ( 1 )
 	{
+		if ( skipRestOfLine ) {
+			// skip unused data for known directives
+			SkipRestOfLineUntilBrace( text );
+		}
+		skipRestOfLine = qtrue;
+
 		token = COM_ParseExt( text, qtrue );
 		if ( !token[0] )
 		{
@@ -1933,11 +1947,13 @@ static qboolean ParseShader( char **text )
 			stages[s].active = qtrue;
 			s++;
 
+			// Don't skip data after the }
+			skipRestOfLine = qfalse;
+
 			continue;
 		}
 		// skip stuff that only the QuakeEdRadient needs
 		else if ( !Q_stricmpn( token, "qer", 3 ) ) {
-			SkipRestOfLine( text );
 			continue;
 		}
 		// sun parms
@@ -1974,7 +1990,6 @@ static qboolean ParseShader( char **text )
 			continue;
 		}
 		else if ( !Q_stricmp( token, "tesssize" ) ) {
-			SkipRestOfLine( text );
 			continue;
 		}
 		else if ( !Q_stricmp( token, "clampTime" ) ) {
@@ -1985,7 +2000,6 @@ static qboolean ParseShader( char **text )
     }
 		// skip stuff that only the q3map needs
 		else if ( !Q_stricmpn( token, "q3map", 5 ) ) {
-			SkipRestOfLine( text );
 			continue;
 		}
 		// skip stuff that only q3map or the server needs
@@ -2067,8 +2081,7 @@ static qboolean ParseShader( char **text )
 			shader.fogParms.density = DEFAULT_FOG_EXP_DENSITY;
 			shader.fogParms.depthForOpaque = atof( token );
 
-			// skip any old gradient directions
-			SkipRestOfLine( text );
+			// note: skips any old gradient directions
 			continue;
 		}
 		// linearFogParms ( <red> <green> <blue> ) <depthForOpaque>
@@ -2087,9 +2100,6 @@ static qboolean ParseShader( char **text )
 			shader.fogParms.fogType = FT_LINEAR;
 			shader.fogParms.density = DEFAULT_FOG_LINEAR_DENSITY;
 			shader.fogParms.depthForOpaque = atof( token );
-
-			// skip any old gradient directions
-			SkipRestOfLine( text );
 			continue;
 		}
 		// portal

code/renderergl2/tr_shader.c
@@ -954,11 +954,18 @@ static qboolean ParseStage( shaderStage_t *stage, char **text, int *ifIndent )
 	char *token;
 	int depthMaskBits = GLS_DEPTHMASK_TRUE, blendSrcBits = 0, blendDstBits = 0, atestBits = 0, depthFuncBits = 0;
 	qboolean depthMaskExplicit = qfalse;
+	qboolean skipRestOfLine = qfalse;
 
 	stage->active = qtrue;
 
 	while ( 1 )
 	{
+		// skip unused data for known parameters, but not the initial {
+		if ( skipRestOfLine ) {
+			SkipRestOfLineUntilBrace( text );
+		}
+		skipRestOfLine = qtrue;
+
 		token = COM_ParseExt( text, qtrue );
 		if ( !token[0] )
 		{
@@ -2081,6 +2088,7 @@ static qboolean ParseShader( char **text )
 	char *token;
 	int s;
 	int ifIndent = 0;
+	qboolean skipRestOfLine = qfalse;
 
 	s = 0;
 
@@ -2093,6 +2101,12 @@ static qboolean ParseShader( char **text )
 
 	while ( 1 )
 	{
+		if ( skipRestOfLine ) {
+			// skip unused data for known directives
+			SkipRestOfLineUntilBrace( text );
+		}
+		skipRestOfLine = qtrue;
+
 		token = COM_ParseExt( text, qtrue );
 		if ( !token[0] )
 		{
@@ -2130,11 +2144,13 @@ static qboolean ParseShader( char **text )
 			stages[s].active = qtrue;
 			s++;
 
+			// Don't skip data after the }
+			skipRestOfLine = qfalse;
+
 			continue;
 		}
 		// skip stuff that only the QuakeEdRadient needs
 		else if ( !Q_stricmpn( token, "qer", 3 ) ) {
-			SkipRestOfLine( text );
 			continue;
 		}
 		// sun parms
@@ -2207,7 +2223,6 @@ static qboolean ParseShader( char **text )
 			continue;
 		}
 		else if ( !Q_stricmp( token, "tesssize" ) ) {
-			SkipRestOfLine( text );
 			continue;
 		}
 		else if ( !Q_stricmp( token, "clampTime" ) ) {
@@ -2218,7 +2233,6 @@ static qboolean ParseShader( char **text )
 		}
 		// skip stuff that only the q3map needs
 		else if ( !Q_stricmpn( token, "q3map", 5 ) ) {
-			SkipRestOfLine( text );
 			continue;
 		}
 		// skip stuff that only q3map or the server needs
@@ -2300,8 +2314,7 @@ static qboolean ParseShader( char **text )
 			shader.fogParms.density = DEFAULT_FOG_EXP_DENSITY;
 			shader.fogParms.depthForOpaque = atof( token );
 
-			// skip any old gradient directions
-			SkipRestOfLine( text );
+			// note: skips any old gradient directions
 			continue;
 		}
 		// linearFogParms ( <red> <green> <blue> ) <depthForOpaque>
@@ -2320,9 +2333,6 @@ static qboolean ParseShader( char **text )
 			shader.fogParms.fogType = FT_LINEAR;
 			shader.fogParms.density = DEFAULT_FOG_LINEAR_DENSITY;
 			shader.fogParms.depthForOpaque = atof( token );
-
-			// skip any old gradient directions
-			SkipRestOfLine( text );
 			continue;
 		}
 		// portal

commit 7ba7ff7f9d23f189ff3ac34f6cfa6dbe78200737

spearmint: Fix COM_Parse line counting
Broke in cef4cd1e60a214b573b51e36fd1942effe696de6

code/qcommon/q_shared.c
@@ -375,7 +375,7 @@ string will be returned if the next token is
 a newline.
 ==============
 */
-static char *SkipWhitespace( char *data, qboolean *hasNewLines ) {
+static char *SkipWhitespace( char *data, int *linesSkipped ) {
 	int c;
 
 	while( (c = *data) <= ' ') {
@@ -383,8 +383,7 @@ static char *SkipWhitespace( char *data, qboolean *hasNewLines ) {
 			return NULL;
 		}
 		if( c == '\n' ) {
-			com_lines++;
-			*hasNewLines = qtrue;
+			*linesSkipped += 1;
 		}
 		data++;
 	}
@@ -464,7 +463,7 @@ int COM_Compress( char *data_p ) {
 char *COM_ParseExt2( char **data_p, qboolean allowLineBreaks, char delimiter )
 {
 	int c = 0, len;
-	qboolean hasNewLines = qfalse;
+	int linesSkipped = 0;
 	char *data;
 
 	data = *data_p;
@@ -482,18 +481,20 @@ char *COM_ParseExt2( char **data_p, qboolean allowLineBreaks, char delimiter )
 	while ( 1 )
 	{
 		// skip whitespace
-		data = SkipWhitespace( data, &hasNewLines );
+		data = SkipWhitespace( data, &linesSkipped );
 		if ( !data )
 		{
 			*data_p = NULL;
 			return com_token;
 		}
-		if ( hasNewLines && !allowLineBreaks )
+		if ( data && linesSkipped && !allowLineBreaks )
 		{
 			// ZTM: Don't move the pointer so that calling SkipRestOfLine afterwards works as expected
 			//*data_p = data;
 			return com_token;
 		}
+
+		com_lines += linesSkipped;
 
 		c = *data;
 