e462f68c04269d2d013afcfd907e5fd6aa53949c
Zack Middleton <zturtleman@gmail.com>
2014-03-14 14:03:37 -0500

    Revert "spearmint: Enable com_ansiColor by default"
    This reverts commit c44949f137598f8a3aabd6e13aade0107fc903af.
    
    This causes stderr.txt on Windows client to have ANSI color codes.
    Dedicated server would probably want it off so not creating messy logs.
    Messy logs created by Linux client are annoying too.

--- code/qcommon/common.c
+++ code/qcommon/common.c
@@ -2849,7 +2849,7 @@ void Com_Init( char *commandLine ) {
 	com_sv_running = Cvar_Get ("sv_running", "0", CVAR_ROM);
 	com_cl_running = Cvar_Get ("cl_running", "0", CVAR_ROM);
 	com_buildScript = Cvar_Get( "com_buildScript", "0", 0 );
-	com_ansiColor = Cvar_Get( "com_ansiColor", "1", CVAR_ARCHIVE );
+	com_ansiColor = Cvar_Get( "com_ansiColor", "0", CVAR_ARCHIVE );
 
 	com_unfocused = Cvar_Get( "com_unfocused", "0", CVAR_ROM );
 	com_maxfpsUnfocused = Cvar_Get( "com_maxfpsUnfocused", "0", CVAR_ARCHIVE );
