d85c2092e6ac5d975b0045579dec64e2301f8a98
Zack Middleton <zturtleman@gmail.com>
2015-05-13 16:55:27 -0500

    spearmint: Reduce wallmark z-fighting
    Use Doom 3 values for r_offsetFactor and r_offsetUnits which
    makes wall marks z-fight with other wall marks less often.

--- code/renderergl1/tr_init.c
+++ code/renderergl1/tr_init.c
@@ -1204,8 +1204,8 @@ void R_Register( void )
 	r_shownormals = ri.Cvar_Get ("r_shownormals", "0", CVAR_CHEAT);
 	r_bonesDebug = ri.Cvar_Get ("r_bonesDebug", "0", CVAR_CHEAT);
 	r_clear = ri.Cvar_Get ("r_clear", "0", CVAR_CHEAT);
-	r_offsetFactor = ri.Cvar_Get( "r_offsetfactor", "-1", CVAR_CHEAT );
-	r_offsetUnits = ri.Cvar_Get( "r_offsetunits", "-2", CVAR_CHEAT );
+	r_offsetFactor = ri.Cvar_Get( "r_offsetfactor", "0", CVAR_CHEAT );
+	r_offsetUnits = ri.Cvar_Get( "r_offsetunits", "-600", CVAR_CHEAT );
 	r_drawBuffer = ri.Cvar_Get( "r_drawBuffer", "GL_BACK", CVAR_CHEAT );
 	r_lockpvs = ri.Cvar_Get ("r_lockpvs", "0", CVAR_CHEAT);
 	r_noportals = ri.Cvar_Get ("r_noportals", "0", CVAR_CHEAT);
