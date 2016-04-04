b82062c0bfcfbb44bcf7ace0538c8455e1836049
Zack Middleton <zturtleman@gmail.com>
2016-04-04 12:31:34 -0500

    spearmint: Update linux and osx start_server.sh scripts

--- misc/linux/start_server.sh
+++ misc/linux/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Edit this script to change the path to Spearmint's dedicated server executable and which binary if you aren't on x86_64.\n Set the sv_dlURL setting to a url like http://yoursite.com/spearmint_path for clients to download extra data"
-~/spearmint/spearmint-server_x86_64 +set dedicated 2 +set sv_allowDownload 1 +set sv_dlURL "" +set com_hunkmegs 64 "$@"
+~/spearmint/spearmint-server_x86_64 +set sv_public 1 +set sv_allowDownload 1 +set sv_dlURL "" "$@"
--- misc/osx/start_server.sh
+++ misc/osx/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Edit this script to change the path to Spearmint's dedicated server executable.\n Set the sv_dlURL setting to a url like http://yoursite.com/spearmint_path for clients to download extra data"
-/Applications/Spearmint/Spearmint.app/Contents/MacOS/spearmint-server +set dedicated 2 +set sv_allowDownload 1 +set sv_dlURL "" +set com_hunkmegs 64 "$@"
+/Applications/Spearmint/Spearmint.app/Contents/MacOS/spearmint-server +set sv_public 1 +set sv_allowDownload 1 +set sv_dlURL "" "$@"
