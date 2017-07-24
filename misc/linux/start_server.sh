ff6cc004a56585a1f727759b1b5e17ed30f80cfa
Zack Middleton <zack@cloemail.com>
2017-07-24 17:41:21 -0500

    spearmint: Remove reference in ioquake3 in start_server.sh

--- misc/linux/start_server.sh
+++ misc/linux/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Edit this script to change the path to Spearmint's dedicated server executable and which binary if you aren't on x86_64."
-echo "Set the sv_dlURL setting to a url like http://yoursite.com/spearmint_path for ioquake3 clients to download extra data."
+echo "Set the sv_dlURL setting to a url like http://yoursite.com/spearmint_path for clients to download extra data."
 
 # sv_dlURL needs to have quotes escaped like \"http://yoursite.com/spearmint_path\" or it will be set to "http:" in-game.
 
