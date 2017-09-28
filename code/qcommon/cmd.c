be1bb2725493742725440d88d73840ede06ed3c1
Zack Middleton <zack@cloemail.com>
2017-09-27 23:50:18 -0500

    spearmint: Allow semicolons in chat messages
    Don't remove command separators from client commands processed by Game
    VM. The Game VM callvote exploit has already been fixed.

--- code/qcommon/cmd.c
+++ code/qcommon/cmd.c
@@ -522,31 +522,6 @@ char *Cmd_Cmd(void)
 }
 
 /*
-   Replace command separators with space to prevent interpretation
-   This is a hack to protect buggy qvms
-   https://bugzilla.icculus.org/show_bug.cgi?id=3593
-   https://bugzilla.icculus.org/show_bug.cgi?id=4769
-*/
-
-void Cmd_Args_Sanitize(void)
-{
-	int i;
-
-	for(i = 1; i < cmd.argc; i++)
-	{
-		char *c = cmd.argv[i];
-		
-		if(strlen(c) > MAX_CVAR_VALUE_STRING - 1)
-			c[MAX_CVAR_VALUE_STRING - 1] = '\0';
-		
-		while ((c = strpbrk(c, "\n\r;"))) {
-			*c = ' ';
-			++c;
-		}
-	}
-}
-
-/*
 ============
 Cmd_TokenizeString
 
--- code/qcommon/qcommon.h
+++ code/qcommon/qcommon.h
@@ -526,7 +526,6 @@ char	*Cmd_ArgsFrom( int arg );
 void	Cmd_ArgsBuffer( char *buffer, int bufferLength );
 void	Cmd_LiteralArgsBuffer( char *buffer, int bufferLength );
 char	*Cmd_Cmd (void);
-void	Cmd_Args_Sanitize( void );
 // The functions that execute commands get their parameters with these
 // functions. Cmd_Argv () will return an empty string, not a NULL
 // if arg > argc, so string operations are allways safe.
